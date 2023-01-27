8001e0cc3fd97817d71b0899c75e9b3ea738c7d9
NickIT87 <elanir358@gmail.com>
2023-01-27 17:34:56 +0200

    book example

--- tutorial/hello.hs
+++ tutorial/hello.hs
@@ -1,5 +1,8 @@
 main :: IO ()
 
+rightTriangles' = [ (a,b,c) | c <- [1..10], b <- [1..c], a <- [1..b], a^2 + b^2 == c^2, a+b+c == 24]
+
 main = do
   let message = "hello world"
   putStrLn message
+  print rightTriangles'
