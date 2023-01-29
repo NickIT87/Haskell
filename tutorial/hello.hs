efa86140079e6c971dc14546d1fe728b3a82d4bf
NickIT87 <elanir358@gmail.com>
2023-01-29 21:33:01 +0200

    bmi

--- tutorial/hello.hs
+++ tutorial/hello.hs
@@ -2,7 +2,21 @@ main :: IO ()
 
 rightTriangles' = [ (a,b,c) | c <- [1..10], b <- [1..c], a <- [1..b], a^2 + b^2 == c^2, a+b+c == 24]
 
+bmiTell :: (RealFloat a) => a -> a -> String
+bmiTell weight height  
+    | bmi <= skinny = "You're underweight, you emo, you!"  
+    | bmi <= normal = "You're supposedly normal. Pffft, I bet you're ugly!"  
+    | bmi <= fat    = "You're fat! Lose some weight, fatty!"  
+    | otherwise     = "You're a whale, congratulations!"  
+    where bmi = weight / height ^ 2  
+          skinny = 18.5  
+          normal = 25.0  
+          fat = 30.0 
+
+bmi = bmiTell 100 1.85
+
 main = do
   let message = "hello world"
   putStrLn message
   print rightTriangles'
+  putStrLn bmi
